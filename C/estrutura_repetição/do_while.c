c6f1e939fc75aeb0e91e53a146b4230441cec34a
gus <111452823+gusjjpv@users.noreply.github.com>
2023-10-13 13:12:00 -0300

    Update do_while.c
    correções na anotação

--- C/estrutura_repetição/do_while.c
+++ C/estrutura_repetição/do_while.c
@@ -1,5 +1,5 @@
 /*
-O do while é diferente dos outros laços, porque o teste da condição acontece no final do corpo, ent o do while é executado pelo menos uma vez, enquanto nos outros laços pode nunca ser executado.
+O do while é diferente dos outros laços, porque o teste da condição acontece no final do corpo, ent o do...while é executado pelo menos uma vez, enquanto nos outros laços pode nunca ser executado.
 
 */
 
@@ -16,10 +16,7 @@ main(){
     /*o funcionamento segue esses passos:
         1.A instrução é executada
         2.A condição é avalida
-        3.Se o resultado da condição for Verdade, volta-se ao ponto 1/ se o ltado for Falso, termina o laço e o programa continua na instrução seguinte ao laço.
+        3.Se o resultado da condição for Verdade, volta-se ao ponto 1/ se o resultado for Falso, termina o laço e o programa continua na instrução seguinte ao laço.
     */
 // O laço do...while esta particulamente adaptado ao processamento de menus.
-
-
-
 }
