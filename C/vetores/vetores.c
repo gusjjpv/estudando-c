a87288ead12774c59818e14e5fe271b070cdc62f
gus <111452823+gusjjpv@users.noreply.github.com>
2023-10-13 13:20:22 -0300

    Update vetores.c
    correções nas anotações

--- C/vetores/vetores.c
+++ C/vetores/vetores.c
@@ -8,21 +8,23 @@
 main()
 {
     /*
-        Declaração
-            Um vetor em C é declarado da msm forma que uma variavel simples. A declaração obedece a seguinte sintaxe:
+        Declaração:
+           Um vetor em C é declarado da msm forma que uma variavel simples. 
+        
+        A declaração obedece a seguinte sintaxe:
                 tipo nome_variavel[n° de elementos]
-            tipo-corresponde ao tipo de dados de cada um dos elementos do vetor
-            nome_variavel-indica o nome pelo qual esse vetor vai ser conhecido.
-            n° de elementos-valor constante que indica quantos elementos tem o vetor
+            1. tipo- corresponde ao tipo de dados de cada um dos elementos do vetor
+            2. nome_variavel- indica o nome pelo qual esse vetor vai ser conhecido.
+            3. n° de elementos- valor constante que indica quantos elementos tem o vetor
     */
 
     int g[20]; // vetor do tipo inteiro chamado de g e com 20 numeros inteiros
 
     float x[100]; // vetor do tipo real chamado de x e com 100 numeros reais
     /*
-     qualquer elemento do vetor pode ser identificado como x ou g(exemplos anteriores), mas para identificar cada um deles individualmente é necessario um numero que indique qual sua posição no vetor, que é o que chamaamos de INDICE.
-
-     O indice começa pelo 0, então em um vetor com 20 numeros, o primeiro elementos vai ser o 0, e o ultimo elemento vai ser o 19. Ent vai variar entre 0 e total de elementos-1.
+     qualquer elemento do vetor pode ser identificado como x ou g(exemplos anteriores), mas para identificar cada um deles individualmente 
+     é necessario um numero que indique qual sua posição no vetor, que é o que chamaamos de INDICE. O indice começa pelo 0, então em um vetor com 20 numeros
+     o primeiro elementos vai ser o 0, e o ultimo elemento vai ser o 19. Ent vai variar entre 0 e total de elementos-1.
 
      int vetor[3]
      indice: vetor[0], vetor[1], vetor[2];
@@ -59,7 +61,7 @@ main()
     // É POSSIVEL INICIAR AUTOMATICAMENTE TODOS OS ELEMENTOS DE UM VETOR ATRAVES DA SEGUINTE SINTAXE:
     char vogal[5] = {'a', 'e', 'i', 'o', 'u'};
     // indice =     { 0 ,  1 ,  2 ,  3 ,  4 };
-    //  se um vetor com 5 elementos for incializado somente com 2, os outros 3 serão inicalizados com 0
+    //  se um vetor com 5 elementos for incializado somente com 2 de carga inicial, os outros 3 serão inicalizados com 0
 
 
     //PASSAGEM DE VETORES PARA FUNÇÕES
@@ -74,10 +76,13 @@ main()
             }
         }
 agr todos os valores vão iniciar com 0.
-Ao colocar um vetor como parametro de uma função não é necessario indicar a quantidade de elementos presentes no vetor, mas coloca uma variavel inteira para indicar qual o numero de elementos. Dessa forma é possivel iniciar um vetor de inteirios de qualquer dimensão, pois não é indicado no parametro da função qual o numero de elementos que o vetor contem.
 
-Nota: se for indicar a dimensão do vetor no parametro da função, o numero de elementos do vetor é simlismente ignorado pelo compilador. A função se interessa somente em saber qual o tipo dos elementos do vetor.
+Ao colocar um vetor como parametro de uma função não é necessario indicar a quantidade de elementos presentes no vetor
+coloca-se uma variavel inteira para indicar qual o numero de elementos. Dessa forma é possivel iniciar um vetor de inteirios de qualquer dimensão
+pois não é indicado no parametro da função qual o numero de elementos que o vetor contem.
+
+Nota: se for indicar a dimensão do vetor no parametro da função, o numero de elementos do vetor é simlismente ignorado pelo compilador. 
+A função se interessa somente em saber qual o tipo dos elementos do vetor.
 
         */
-
 }
