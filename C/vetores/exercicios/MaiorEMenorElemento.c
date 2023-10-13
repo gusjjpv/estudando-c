045cf96593729576c1549977ca855887b1f2db06
gus <111452823+gusjjpv@users.noreply.github.com>
2023-10-13 13:38:24 -0300

    Update MaiorEMenorElemento.c
    exercício concluído e rodando, fiz com números dados pelo usuário, achei mais interessante

--- C/vetores/exercicios/MaiorEMenorElemento.c
+++ C/vetores/exercicios/MaiorEMenorElemento.c
@@ -3,23 +3,30 @@
 
 main()
 {
-    int numeros[10], maior = 0, menor, i;
-
-    printf("informe um numero: \n");
-    scanf("%d", &menor);
-    numeros[0] = menor;
-
-    for (i = 1; i < 10; i++)
-    {
-        printf("Informe um numero: \n");
-        scanf("%d", &numeros[i]);
-    }
+    int vetor[10];
+    int i, maior, menor;
 
     for (i = 0; i < 10; i++)
     {
-        if (numeros[i] > numeros[i + 1])
-        {
-           if(numeros[i] > numeros[i + 2])
+        printf("Informe um numero: \n");
+        scanf("%d", &vetor[i]);
+    }
+
+    /*for(i=0;i<10;i++){
+        printf("%d-%d\n",i+1, vetor[i]);
+    }*/
+
+    maior = menor = vetor[0]; //Inicialize as variáveis maior e menor com o primeiro elemento do vetor
+
+    for(i=1;i<10;i++){
+        if(vetor[i]>maior){
+            maior = vetor[i];
+        }
+        if(vetor[i] < menor){
+            menor = vetor[i];
         }
     }
+
+    printf("maior elemento é %d\n",maior);
+    printf("menor elemento é %d\n",menor);
 }
