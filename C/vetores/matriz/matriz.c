e46dfb36548ab2d879c8dcbf0f69421272a88f97
Joao Gustavo <joaogustavolima084@gmail.com>
2023-11-28 22:25:01 -0300

    manipulando matrices

--- C/vetores/matriz/matriz.c
+++ C/vetores/matriz/matriz.c
@@ -2,14 +2,26 @@
 
 int main()
 {
-
+    // matriz de 3 linhas e 3 colunas
     int mat[3][3];
     int i, j;
+    int velha[3][3] = {1, 2, 3};
 
-    char velha[3][3] = {',',',',',',',',',',',',',',',',',',','};
-    
+    int m[3][3] = {{1, 2, 3},
+                   {4, 5, 6},
+                   {7, 8, 9}};
+
     for(i = 0;i<3;i++){
-        for(j = 0; j < 3; j++){
+        for(j = 0; j<3;j++){
+            printf("%d", m[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
             printf("%c", velha[i][j]);
         }
         printf("\n");
@@ -30,5 +42,6 @@ int main()
         }
         printf("\n");
     }
+    printf("%d", mat[0][1]);
     return 0;
 }
